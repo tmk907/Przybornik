779370db07f9d54321477d2bcfe3becc71a2d8a2
Tomasz Walecki <tomek_w@outlook.com>
2019-11-29 12:30:45 +0100

    Add single file templates

---
+++ Visual Studio/Templates/MediatR Single File/CommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace $rootnamespace$
+{
+    public class $fileinputname$Command : IRequest
+    {
+        
+    }
+
+    public class $fileinputname$CommandHandler : IRequestHandler<$fileinputname$Command>
+    {
+        public Task<Unit> Handle($fileinputname$Command request, CancellationToken cancellationToken)
+        {
+            return Unit.Task;
+        }
+    }
+}
---
+++ Visual Studio/Templates/MediatR Single File/QueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace $rootnamespace$
+{
+    public class $fileinputname$Query : IRequest<$fileinputname$Response>
+    {
+    }
+
+    public class $fileinputname$Response
+    {
+    }
+
+    public class $fileinputname$QueryHandler : IRequestHandler<$fileinputname$Query, $fileinputname$Response>
+    {
+        public Task<$fileinputname$Response> Handle($fileinputname$Query request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
